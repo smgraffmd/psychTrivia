06269dd994806ddf3444268ce2075eb714fce819
Sarah Graff <sta12s@gmail.com>
2020-07-22 15:17:25 -0400

    changed port

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const { isValidString } = require("./utils/validate");
 const { getCategories, shuffleArray } = require("./utils/questions");
 
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 const app = express();
 const publicPath = path.join(__dirname, "..", "public");
 var server = http.createServer(app);
